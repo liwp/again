ab86ff0e7b4d14f80623e165519f35cd97a9d0b2
Lauri Pesonen <lauri.pesonen@iki.fi>
2014-08-07 13:31:32 +0100

    Add clojars/release fields to project.clj

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "https://github.com/listora/again"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-
+  :scm {:name "git"
+        :url "https://github.com/listora/again"}
+  :deploy-repositories [["clojars" {:creds :gpg}]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.5.9"]
                                   [org.clojure/clojure "1.6.0"]]}})
